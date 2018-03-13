eee7a434a350377bdeb4aac60a0fd5678e5a2e4c
祝飞 <ZhuFeiWhu@outlook.com>
2018-03-13 09:55:27 +0800

    新增归并排序算法

--- Learncpp/Learncpp.cpp
+++ Learncpp/Learncpp.cpp
@@ -12,8 +12,9 @@ void insertSort(vector<int> &a);
 
 int main()
 {
-	vector<int> A{ 3,6,2,8,4,8,23 };
-	insertSort(A);
+	vector<int> A{3,6,2,8,4,8,23 ,10,15};
+	//insertSort(A);
+	MergeSort(A);
 	for (int i = 0; i < A.size(); i++)
 	{
 		cout << A[i] << " ";
--- Learncpp/Sort.h
+++ Learncpp/Sort.h
@@ -1,7 +1,11 @@
 #pragma once
-#include <vector>
+//排序算法汇总，各种排序算法的实现c++
 
-//插入排序算法
+//归并排序，函数申明
+std::vector <int> Merge(std::vector<int> l, std::vector<int> r);
+
+
+//插入排序
 void insertSort(std::vector<int> &a)
 {
 	int key;
@@ -19,4 +23,47 @@ void insertSort(std::vector<int> &a)
 	}
 }
 
-//shell 排序
+//归并排序
+void MergeSort(std::vector<int> &a)
+{
+	int n = a.size();
+	int mid = n / 2;
+	std::vector <int> Left, Right,b;
+	std::vector <int> Result(n);
+	if (mid < 1)
+		return;
+	//赋值操作
+	Left.assign(a.begin(), a.begin() + mid);
+	Right.assign(a.begin() + mid, a.end());
+	MergeSort(Left);
+	MergeSort(Right);
+	a = Merge(Left, Right);
+}
+//对归并的两个分支进行排序
+std::vector<int> Merge(std::vector<int> l, std::vector<int> r)
+{
+	int m = l.size();
+	int n = r.size();
+	//1.设置最大值的哨兵
+	l.push_back(100000000000);
+	r.push_back(100000000000);
+	std::vector <int> Result(m+n);
+	int i = 0, j = 0;
+	for (int k =0 ; k < Result.size(); k++)
+	{
+		if (l[i] <= r[j])
+		{
+			Result[k]=l[i];
+			i = i + 1;
+		}
+		else
+		{
+			Result[k] = r[j];
+			j = j + 1;
+		}
+	}
+	return Result;
+	//2.采用插入排序的方法
+}
+
+// shell sort
